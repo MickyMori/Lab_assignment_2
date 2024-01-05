c104c6953f6f9a6b42a4d07c2ff5afc24ad80ddb
Michele Moriconi <102163784+MickyMori@users.noreply.github.com>
2024-01-05 15:28:24 +0100

    Add files via upload

--- lab_assignment_2/src/actions/go_to_waypoint.cpp
+++ lab_assignment_2/src/actions/go_to_waypoint.cpp
@@ -1,4 +1,4 @@
-#include "my_rosplan_interface/my_action.h" 
+#include "lab_assignment_2/my_action.h" 
 #include <unistd.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
