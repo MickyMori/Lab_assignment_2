c5dc7b27bd930713f92f18d8050e8ac90ff3356b
Michele Moriconi <102163784+MickyMori@users.noreply.github.com>
2024-01-06 16:35:07 +0100

    Add files via upload

--- lab_assignment_2/src/find_marker_action_server.cpp
+++ lab_assignment_2/src/find_marker_action_server.cpp
@@ -2,13 +2,22 @@
 #include <actionlib/server/simple_action_server.h>
 #include <lab_assignment_2/FindMarkerAction.h>
 #include <std_msgs/Int32.h>
+#include <geometry_msgs/Twist.h>
 
 class FindMarkerActionServer {
 public:
     FindMarkerActionServer(ros::NodeHandle nh) : nh_(nh), as_(nh, "find_marker", boost::bind(&FindMarkerActionServer::executeCallback, this, _1), false) {
         as_.start();
         marker_pub_ = nh_.advertise<std_msgs::Int32>("/rosbot/search_id", 10);
+        cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
         found_sub_ = nh_.subscribe("/rosbot/marker_found", 1, &FindMarkerActionServer::foundCallback, this);
+    }
+
+    void roatate_rosbot(double ang_z)
+    {
+        geometry_msgs::Twist cmd_vel_msg;
+        cmd_vel_msg.angular.z = ang_z;
+        cmd_vel_pub.publish(cmd_vel_msg);
     }
 
     void executeCallback(const lab_assignment_2::FindMarkerGoalConstPtr &goal) {
@@ -16,11 +25,15 @@ public:
         std_msgs::Int32 id = goal;
         marker_pub_.publish(id);
 
+        rotate_rosbot(0.8);
+
         // Wait for marker found
         while (!marker_found_) {
             ros::spinOnce();
             ros::Duration(0.1).sleep();  // adjust sleep duration as needed
         }
+
+        roatate_rosbot(0.0);
 
         lab_assignment_2::FindMarkerResult result;
         result.result = true;
