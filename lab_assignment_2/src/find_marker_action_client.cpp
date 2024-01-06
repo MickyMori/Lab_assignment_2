f652160e5b815e99d1eb653a22e828a51fe0f250
GioRocca <58571812+GioRocca@users.noreply.github.com>
2024-01-06 15:53:12 +0000

    Update find_marker_action_client.cpp

--- lab_assignment_2/src/find_marker_action_client.cpp
+++ lab_assignment_2/src/find_marker_action_client.cpp
@@ -6,6 +6,7 @@
 #include <move_base_msgs/MoveBaseActionGoal.h>
 #include <move_base_msgs/MoveBaseGoal.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/Int32.h>
 
 typedef actionlib::SimpleActionClient<lab_assignment_2::FindMarkerAction> FindMarkerClient;
 
